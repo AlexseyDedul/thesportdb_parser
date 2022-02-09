d04579a6693cee222aba1984acfb6d0bbfd7fe0f
AlexseyDedul <dedyul@gmail.com>
2022-02-09 12:57:28 +0300

    updated module

--- request.py
+++ request.py
@@ -3,9 +3,7 @@ Module to Provide Functionality for Making requests to the API.
 """
 from __future__ import absolute_import
 import settings as TSD
-import requests
 import aiohttp
-import asyncio
 
 
 def _make_url(endpoint: str) -> str:
@@ -15,12 +13,6 @@ def _make_url(endpoint: str) -> str:
 async def make_request(endpoint: str, **kwargs):
     params = kwargs
     URL = _make_url(endpoint)
-    print(URL)
     async with aiohttp.ClientSession() as session:
         async with session.get(URL, params=params) as response:
-            print(await response.json())
-            return response
-
-    #
-    # response = requests.get(URL, params=params)
-    # return response.json()
+            return await response.json()
