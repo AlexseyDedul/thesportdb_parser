2cad647808feabbe945b57246e900ece43b67f7b
dedul.an <dedul.an@prj365.com>
2022-04-18 13:28:47 +0300

    final

--- app/thesportsdb_parser/main_thesporsdb.py
+++ app/thesportsdb_parser/main_thesporsdb.py
@@ -2,7 +2,6 @@ import asyncio
 
 import asyncpg
 
-from app import create_app
 from app.thesportsdb_parser.contracts import insert_contracts
 from app.thesportsdb_parser.countries import insert_countries
 from app.thesportsdb_parser.eventTV import insert_events_tv
@@ -12,7 +11,7 @@ from app.thesportsdb_parser.honourTeam import insert_honours_teams
 from app.thesportsdb_parser.lastEvents import work_with_last_events
 from app.thesportsdb_parser.leagues import insert_leagues, get_leagues_ids_list
 from app.thesportsdb_parser.lineup import insert_lineups
-from app.thesportsdb_parser.nextEvents import get_next_events_api, work_with_events
+from app.thesportsdb_parser.nextEvents import work_with_events
 from app.thesportsdb_parser.players import get_list_players_db, insert_players
 from app.thesportsdb_parser.sports import insert_sports
 from app.thesportsdb_parser.tables import insert_tables
@@ -47,12 +46,12 @@ async def start_parser(pool: asyncpg.pool.Pool):
 async def tasks_once_a_month(pool: asyncpg.pool.Pool):
     while True:
         await asyncio.sleep(60 * 60 * 24 * 30)
-        await asyncio.create_task(insert_countries(pool, await allCountries())),
-        await asyncio.create_task(insert_sports(pool, await allSports())),
-        await asyncio.create_task(insert_leagues(pool, await allLeagues()))
+        await insert_countries(pool, await allCountries()),
+        await insert_sports(pool, await allSports()),
+        await insert_leagues(pool, await allLeagues())
         leagues = await get_leagues_ids_list(pool)
-        await asyncio.create_task(insert_teams(pool, leagues)),
-        await asyncio.create_task(insert_players(pool))
+        await insert_teams(pool, leagues),
+        await insert_players(pool)
         players = await get_list_players_db(pool)
         await asyncio.gather(
             asyncio.create_task(insert_contracts(pool, players)),
@@ -65,14 +64,14 @@ async def tasks_once_a_week(pool: asyncpg.pool.Pool):
     while True:
         await asyncio.sleep(60 * 60 * 24 * 7)
         leagues = await get_leagues_ids_list(pool)
-        await asyncio.create_task(insert_tables(pool, leagues))
+        await insert_tables(pool, leagues)
 
 
 async def tasks_once_a_day(pool: asyncpg.pool.Pool):
     while True:
         await asyncio.sleep(60 * 60 * 24)
-        asyncio.create_task(work_with_events(pool)),
-        asyncio.create_task(work_with_last_events(pool))
+        await work_with_events(pool),
+        await work_with_last_events(pool)
 
 
 async def main(app):
@@ -83,10 +82,11 @@ async def main(app):
     await start_parser(pool)
 
     tasks = [
-            asyncio.create_task(tasks_once_a_month(pool)),
-            asyncio.create_task(tasks_once_a_week(pool)),
-            asyncio.create_task(tasks_once_a_day(pool))
+        asyncio.create_task(tasks_once_a_month(pool)),
+        asyncio.create_task(tasks_once_a_week(pool)),
+        asyncio.create_task(tasks_once_a_day(pool))
         ]
+
     await asyncio.gather(
         *tasks
     )
